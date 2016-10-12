efa92f2695c3955adfcbe7956466fee2421edf54
mclavelguzman <mclavelguzman@gmail.com>
2016-10-12 14:32:24 -0600

    add edit and list to my notes

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,6 @@
+(function() {
+
 var app = angular.module('starter', ['ionic']);
-
-app.controller('ListCtrl', function($scope) {
-
-  $scope.notes = [
-    {
-      title: 'First Note',
-      description: 'This is my first note.'
-    },
-    {
-      title: 'Second Note',
-      description: 'This is my second note.'
-    }
-  ];
-});
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
@@ -22,20 +10,69 @@ app.config(function($stateProvider, $urlRouterProvider) {
   });
 
   $stateProvider.state('edit', {
-    url: '/edit',
+    url: '/edit/:noteId',
     templateUrl: 'templates/edit.html'
   });
 
-  $urlRouterProvider.otherwise('/list')
+  $urlRouterProvider.otherwise('/list');
+});
+
+var notes = [
+  {
+    id: '1',
+    title: 'First Note',
+    description: 'This is my first note.'
+  },
+  {
+    id: '2',
+    title: 'Second Note',
+    description: 'This is my second note.'
+  }
+];
+
+function getNote(noteId) {
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].id === noteId) {
+      return notes[i];
+    }
+  }
+  return undefined;
+}
+
+function updateNote(note) {
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].id === note.id) {
+      notes[i] = note;
+      return;
+    }
+  }
+}
+
+app.controller('ListCtrl', function($scope) {
+
+  $scope.notes = notes;
+
+});
+
+app.controller('EditCtrl', function($scope, $state) {
+
+  $scope.note = angular.copy(getNote($state.params.noteId));
+
+  $scope.save = function() {
+    updateNote($scope.note);
+    $state.go('list');
+  };
 });
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+}());
